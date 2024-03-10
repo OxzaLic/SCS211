0561751903dc65ffe45ba70fe3c86359df02bbf5
OxzaLic <152046308+OxzaLic@users.noreply.github.com>
2024-03-10 11:54:44 +0700

    Add files via upload

---
+++ week11/quiz1.php
@@ -0,0 +1,10 @@
+<?php
+$people_obj = json_decode(file_get_contents("https://raw.githubusercontent.com/arc6828/SCS211/main/week13/staff.json"),true);
+$people = $people_obj["people"];
+?>
+
+<?php foreach($people as $row){ ?>
+    <div><?=$row["name"]?></div>
+
+
+<?php } ?>
