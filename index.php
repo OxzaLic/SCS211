bc49f380b6048d6fb0310a9ec66811986628f33a
OxzaLic <152046308+OxzaLic@users.noreply.github.com>
2024-03-10 13:43:40 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Dashboard</title>
+    <?php include('layouts/employee-style.php'); ?>
+    <meta charset="UTF-8">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <?php include('layouts/basic-nav.php');?>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="mt-5 mb-3 clearfix">
+                        <h2 class="pull-left">This is Home Page </h2>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php include('layouts/employee-script.php'); ?>
+</body>
+
+</html>
